55d4b161bc96b4a6b738f39beeb85196eef771ec
ImRLopezAG <angel178293lopez@gmail.com>
2023-09-02 22:40:39 -0400

    add map

--- src/components/pages/components/ProjectCard.tsx
+++ src/components/pages/components/ProjectCard.tsx
@@ -1,10 +1,10 @@
-import { AspIcon, BootstrapIcon, CsharpIcon, CssIcon, DartIcon, DotNetIcon, ExpressIcon, FlutterIcon, GitIcon, HTMLIcon, JSIcon, MongoDbIcon, NextJsIcon, NodeIcon, ReactIcon, SequelizeIcon, SqlIcon, TSIcon, TailwindIcon } from '@/components/icon'
 import { Card, CardBody, CardFooter, CardHeader } from '@nextui-org/card'
 import { Divider } from '@nextui-org/divider'
 import { Link } from '@nextui-org/link'
 import NextLink from 'next/link'
 import CardModal from './CardModal'
 import type { TechTypes } from '@/types'
+import { Map } from '@/components/icon/map'
 
 interface ProjectCardProps {
   title: string
@@ -16,32 +16,11 @@ interface ProjectCardProps {
 }
 
 export const ProjectCard: React.FC<ProjectCardProps> = ({ title, description, repo, tech, techs, images }) => {
-  const technologies: Record<TechTypes, JSX.Element> = {
-    '.Net': <DotNetIcon />,
-    'Asp.Net': <AspIcon />,
-    Bootstrap: <BootstrapIcon />,
-    'C-Sharp': <CsharpIcon />,
-    CSS: <CssIcon />,
-    Git: <GitIcon />,
-    HTML: <HTMLIcon />,
-    JavaScript: <JSIcon />,
-    MongoDb: <MongoDbIcon />,
-    NextJs: <NextJsIcon />,
-    Node: <NodeIcon />,
-    React: <ReactIcon />,
-    Sequelize: <SequelizeIcon />,
-    SQL: <SqlIcon />,
-    TailwindCss: <TailwindIcon />,
-    TypeScript: <TSIcon />,
-    Dart: <DartIcon />,
-    Flutter: <FlutterIcon />,
-    Express: <ExpressIcon />
-  }
   return (
     <Card className='min-w-[22rem] flex-col items-center'>
       <CardHeader className='flex gap-3'>
         <div className='h-16 w-16 flex justify-center'>
-          {technologies[tech]}
+          <Map tech={tech} />
         </div>
         <p className='text-md'>{title}</p>
       </CardHeader>
--- src/components/pages/components/Technologies.tsx
+++ src/components/pages/components/Technologies.tsx
@@ -1,36 +1,35 @@
-import { AspIcon, BootstrapIcon, CsharpIcon, CssIcon, DartIcon, DotNetIcon, ExpressIcon, FlutterIcon, GitIcon, HTMLIcon, JSIcon, MongoDbIcon, NextJsIcon, NodeIcon, ReactIcon, SequelizeIcon, SqlIcon, TSIcon, TailwindIcon } from '@/components/icon'
 import { Tooltip } from '@nextui-org/tooltip'
-
 import type { TechTypes } from '@/types'
+import { Map } from '@/components/icon/map'
 
 export const Technologies = (): JSX.Element => {
-  const technologies: Record<TechTypes, JSX.Element> = {
-    '.Net': <DotNetIcon />,
-    'Asp.Net': <AspIcon />,
-    Bootstrap: <BootstrapIcon />,
-    'C-Sharp': <CsharpIcon />,
-    CSS: <CssIcon />,
-    Git: <GitIcon />,
-    HTML: <HTMLIcon />,
-    JavaScript: <JSIcon />,
-    MongoDb: <MongoDbIcon />,
-    NextJs: <NextJsIcon />,
-    Node: <NodeIcon />,
-    React: <ReactIcon />,
-    Sequelize: <SequelizeIcon />,
-    SQL: <SqlIcon />,
-    TailwindCss: <TailwindIcon />,
-    TypeScript: <TSIcon />,
-    Dart: <DartIcon />,
-    Flutter: <FlutterIcon />,
-    Express: <ExpressIcon />
-  }
+  const technologies: TechTypes[] = [
+    '.Net',
+    'Asp.Net',
+    'Bootstrap',
+    'C-Sharp',
+    'CSS',
+    'Git',
+    'HTML',
+    'JavaScript',
+    'MongoDb',
+    'NextJs',
+    'Node',
+    'React',
+    'Sequelize',
+    'SQL',
+    'TailwindCss',
+    'TypeScript',
+    'Dart',
+    'Flutter',
+    'Express'
+  ]
   return (
     <div className='flex gap-3 min-[420px]:flex-wrap overflow-auto max-[420px]:flex-row hide-scrollbar'>
-      {Array.from(Object.keys(technologies)).map((tech) => (
+      {technologies.map((tech) => (
         <Tooltip key={tech} color='primary' content={tech}>
         <div className='bg-slate-700/40 p-2 rounded-md h-16 w-16 flex justify-center scale-up-top-left'>
-          {technologies[tech as TechTypes]}
+          <Map tech={tech} />
         </div>
       </Tooltip>
       ))}
