eaa8bbcb02e185826496c114eb9c03b81dde359d
ImRLopezAG <angel178293lopez@gmail.com>
2023-09-03 13:27:33 -0400

    remove dot

--- src/components/pages/Profile.tsx
+++ src/components/pages/Profile.tsx
@@ -4,7 +4,7 @@ export const Profile = (): JSX.Element => {
   return (
     <div className='flex max-[420px]:flex-col gap-20 items-start min-[420px]:mt-4 [&>section]:flex-col [&>section>h2]:font-bold [&>section>h2]:my-4 [&>section]:max-[420px]:w-full '>
       <section className='w-1/2 flex max-[420px]:hidden'>
-          <h2 className='text-3xl type-blinking'>Angel <span className='text-blue-500'>Gabriel</span> Lopez.</h2>
+          <h2 className='text-3xl type-blinking'>Angel <span className='text-blue-500'>Gabriel</span> Lopez</h2>
         <p className='text-lg'>
         <span className='text-blue-500'>Software Engineer</span> and{' '}
           <span className='text-blue-500'>polyglot developer</span> with +2
